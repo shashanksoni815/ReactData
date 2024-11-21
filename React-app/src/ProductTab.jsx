79d0184152098915c9af19d663962ea8a7b23063
Shashank Soni <shashanksoni815@gmail.com>
2024-11-21 23:03:59 +0530

    react

--- React-app/src/ProductTab.jsx
+++ React-app/src/ProductTab.jsx
@@ -1,7 +1,7 @@
 import Product from "./Product"
 import './Product.css';
 
-function ProductTab(Title) {
+function ProductTab(Title, Description) {
     return (
             <>
             <Product />
