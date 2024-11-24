c4172ea1017e79db161589f64fde8f2ad94b509f
Shashank Soni <shashanksoni815@gmail.com>
2024-11-24 22:16:40 +0530

    jsx

--- React-app/src/App.jsx
+++ React-app/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css';
 import ProductTab from "./ProductTab";
 // import MsgBox from './MsgBox';
+import Button from './Button';
 
 function App() {
   return (
-  <>
-      <h2>BlockBuster Deals | Shop Now</h2>
-    <ProductTab />
-  </>
+    <>
+      <Button />
+    </>
+
+
+  // <>
+  //     <h2>BlockBuster Deals | Shop Now</h2>
+  //   <ProductTab />
+  // </>
   )
 }
 
