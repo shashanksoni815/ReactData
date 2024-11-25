52e3307bc3532dfede40f0bf535c1eb93d7dcf37
Shashank Soni <shashanksoni815@gmail.com>
2024-11-25 16:56:48 +0530

    react

--- React-app/src/Button.jsx
+++ React-app/src/Button.jsx
@@ -1,7 +1,16 @@
+function printHello() {
+    console.log("Hello World")
+}
+
+function printBye() {
+    console.log("byee")
+}
+
 export default function Button() {
     return (
         <div>
-            <button>Click me!</button>
+            <button onClick={printHello}>Click me!</button>
+            <p onClick={printBye}> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores sed dolore nostrum voluptates hic fuga autem? Voluptas rerum facere non adipisci, libero reprehenderit eos fugiat dolore obcaecati inventore cum modi.</p>
         </div>
     )
 }
