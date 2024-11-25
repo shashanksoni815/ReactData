a434b363f91787aea5070c8941ba9a358ba8b68e
Shashank Soni <shashanksoni815@gmail.com>
2024-11-26 00:37:51 +0530

    react

--- React-State/src/Counter.jsx
+++ React-State/src/Counter.jsx
@@ -1,14 +1,19 @@
-export default function Counter() {
-    let count = 0;
+import { useState } from "react";
 
-    function inCount() {
-        count +=1;
-        console.log(count);
-    } 
+export default function Counter() {
+    // let [stateVariable, setStateVariable] = useState(10);
+    let [count, setCount] = useState(0)
+
+    let inCount = () => {
+        //count = count + 1;
+        setCount(count+1);
+        // console.log(count);
+    };
 
     return (
         <div>
             <h3>Count = {count}</h3>
+            <button onClick={inCount}>Increse Count</button>
         </div>
     )
 }
