27eb450fec2b43256d341d11088cfb7fa7e6e5eb
Shashank Soni <shashanksoni815@gmail.com>
2024-11-27 23:44:48 +0530

    btn

--- React-State/src/LikeBtn.jsx
+++ React-State/src/LikeBtn.jsx
@@ -1,5 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { byPrefixAndName } from '@awesome.me/kit-KIT_CODE/icons'
+
 export default function LikeBtn() {
     <div>
-        <p><i class="fa-regular fa-heart"></i></p>
+        {/* <p><FontAwesomeIcon icon={byPrefixAndName.fas['house']} /></p> */}
+        <p><FontAwesomeIcon icon="fa-regular fa-heart" /></p>
+        
     </div>
 }
