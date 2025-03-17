2a09aa1e3e7eaaebd12343795119017b61fab906
Shashank Soni <shashanksoni815@gmail.com>
2025-03-17 20:10:53 +0530

    ToDo

--- events/src/App.jsx
+++ events/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Counter from './Counter'
 import Like from './Like'
 import Ludo from './Ludo'
+import TodoList from './TodoList'
 // import Button from './Button'
 // import Form from './Form'
 
@@ -16,7 +17,7 @@ function App() {
       {/* // <h1>States in react</h1>
       // <Counter /> 
       <Like /> */}
-      <Ludo />
+      <TodoList />
     </>
   )
 }
---
+++ events/src/TodoList.jsx
@@ -0,0 +1,12 @@
+function TodoList() {
+    return ( 
+        <>
+            <input placeholder="add a task" />
+            <button>Add Task</button>
+            <br /><br /><br /><br /><br /><br />
+            <h4>Tasks to do</h4>
+        </>
+     );
+}
+
+export default TodoList;
