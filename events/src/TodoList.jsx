19ca60bc9ec7fdac806f49643850b13bd5a83fa6
Shashank Soni <shashanksoni815@gmail.com>
2025-03-17 20:57:45 +0530

    ToDo

--- events/src/TodoList.jsx
+++ events/src/TodoList.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
+
 function TodoList() {
+    let [todos, setTodos] = useState(["sample Tasks"]);
+    let [newTodo, setNewTodo] = useState("");
+
+    let addNewTask = () => {
+        setTodos([...todos, newTodo]);
+        setNewTodo("");
+    }
+
+    let updateTodoValue = (event) => {
+        setNewTodo(event.target.value);
+    }
+
     return ( 
         <>
-            <input placeholder="add a task" />
-            <button>Add Task</button>
+            <input placeholder="add a task" value={newTodo} onChange={updateTodoValue} />
+            <button onClick={addNewTask} >Add Task</button>
             <br /><br /><br /><br /><br /><br />
             <h4>Tasks to do</h4>
+
+            <ul>
+                {
+                    todos.map((todo) => (
+                        <li>{todo}</li>
+                    ))
+                }
+            </ul>
+
         </>
      );
 }
