0638a4884307db1c5edfa5ecfac93a931748ade3
Shashank Soni <shashanksoni815@gmail.com>
2025-03-30 01:03:23 +0530

    api

--- weather-app/src/SearchBox.jsx
+++ weather-app/src/SearchBox.jsx
@@ -1,3 +1,31 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import '../src/SearchBox.css'
+import { useState } from 'react';
+
 export default function SearchBox () {
-    <div><h3>Search the Weather</h3></div>
+    const [city, setCity] = useState("");
+
+    const handleChange = (evt) => {
+        setCity(evt.target.value)
+    }
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+        console.log(city);
+        setCity("")
+    }
+
+    return(
+        <>
+        <div className='container'><h3>Search the Weather</h3></div>
+
+        <form className='container' onChange={handleChange}>
+        <TextField id="city" label="City Name" variant="standard" required value={city}/> <br />
+        <Button variant="contained" type='submit' onClick={handleSubmit}>
+            Send
+        </Button>
+        </form>
+        </>
+    ) 
 }
