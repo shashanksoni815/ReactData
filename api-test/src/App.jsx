786f525827dfff1a7fbbeee4523f1c3ae9ddd379
Shashank Soni <shashanksoni815@gmail.com>
2025-04-02 10:33:21 +0530

    api

--- api-test/src/App.jsx
+++ api-test/src/App.jsx
@@ -1,26 +1,28 @@
 // import { useState } from 'react'
-
+import data from "../public/json/data.json"
 import './App.css'
 import Body from './Pixcel/Body';
+import Cart from "./Pixcel/Cart";
 
 function App() {
-   
-//    apiData.map((data) => {
-//      console.log(data)
-//    })
-//    console.log(api)
-//    localStorage.setItem("apiData", JSON.stringify(apiData))
-//    let user = {name:"Alice", age:25};      "price": "RS 80,000",
-//    localStorage.setItem(user,JSON.stringify(user))
-//   const apiData = JSON.parse(localStorage.getItem("apiData"));
-//   console.log(apiData);
-
+   localStorage.setItem("data", JSON.stringify(data));
   return (
     <>
       Data
-      <Body data={apiData} />
+      <Body apidata={data} />
+      <Cart apiData={data} />
     </>
   )
 }
 
 export default App
+
+
+//  data.map((data) => {
+  //    console.log(data.id)
+  //  })
+//    console.log(api)
+//    let user = {name:"Alice", age:25};      "price": "RS 80,000",
+//    localStorage.setItem(user,JSON.stringify(user))
+//   const apiData = JSON.parse(localStorage.getItem("apiData"));
+//   console.log(apiData);
--- api-test/src/Pixcel/Body.jsx
+++ api-test/src/Pixcel/Body.jsx
@@ -1,17 +1,17 @@
 // import Cart from "./Cart";
 
-function Body({data}) {
+function Body({apidata}) {
 
 
     return ( 
         <>
             <div style={{ display:"flex", flexWrap:"wrap" , justifyContent:"space-between", }}>
                 {
-                    data.map((api) => (
+                    apidata.map((api) => (
                         <div key={api.id} style={{border:"2px solid black", height:"200px", marginTop:"6px",  width:"200px"}}>
                             <h2>{api.name}</h2>
                             <h3>{api.price}</h3>
-                            <button  > Add to cart</button>
+                            <button id={api.id} onClick={addToCart } > Add to cart</button>
                         </div>
                     ))
                 }
