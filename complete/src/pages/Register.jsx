afbbbe6b0bc349344778ba56b199e8e00073e09b
Shashank Soni <shashanksoni815@gmail.com>
2025-04-08 03:17:39 +0530

    remaning

---
+++ complete/src/pages/Register.jsx
@@ -0,0 +1,19 @@
+function Register() {
+    return ( 
+        <>
+            <section>
+                <main>
+                    <div className="section-registeration">
+                        <div className="container grid grid-two-cols">
+                            <div className="registeation-image">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            </section>
+        </>
+     );
+}
+
+export default Register;
