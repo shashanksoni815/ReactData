9ecffe616a4a9ce67a0dd354f38f05c9c287b631
Shashank Soni <shashanksoni815@gmail.com>
2025-07-21 00:00:22 +0530

    data

--- todo/src/Home.jsx
+++ todo/src/Home.jsx
@@ -1,9 +1,22 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home() {
+    // const [todo, setTodo] = useState[""]
+
+    // const handleTodo = (e) => {
+    //     console.log(e.target.value)
+    //     // console.log(todo)
+    //     // console.log(setTodo)
+    // }
+
     return (
+        <>
         <h1>Hello</h1>
+        <input placeholder='enter your todo' onChange={data} /> 
+        <button value={data} onClick={handleTodo} >Submit</button>
+        </>
+        
     );
 }
 
