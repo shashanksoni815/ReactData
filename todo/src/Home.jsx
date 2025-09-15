8ae44cb06baf86588d08327d0f5d1f2773a0cd5d
Shashank Soni <shashanksoni815@gmail.com>
2025-09-15 14:21:16 +0530

    homew

--- todo/src/Home.jsx
+++ todo/src/Home.jsx
@@ -2,19 +2,39 @@
 import React, { useState } from 'react';
 
 function Home() {
-    // const [todo, setTodo] = useState[""]
+    const [todo, setTodo] = useState(["setdata"]);
+    const [task, setTask] = useState("");
 
-    // const handleTodo = (e) => {
-    //     console.log(e.target.value)
-    //     // console.log(todo)
-    //     // console.log(setTodo)
-    // }
+    const handleTodo = () => {
+        console.log(task);
+        setTodo([...todo, task]);
+        setTask('')
+
+        
+        
+    }
+
+    let inpValue = (e) => {
+        // console.log(e)
+        // console.log(e.target)
+        // console.log(e.target.value)
+        setTask(e.target.value)
+
+    }
 
     return (
         <>
         <h1>Hello</h1>
-        <input placeholder='enter your todo' onChange={data} /> 
-        <button value={data} onClick={handleTodo} >Submit</button>
+        <input value={task} placeholder='enter your todo' onChange={inpValue} /> 
+        <button  onClick={handleTodo} >Submit</button>
+        <hr />
+        <div>
+            {
+                todo.map((todos, idx) => (
+                    <li key={idx} >{todos}</li>
+                ))
+            }
+        </div>
         </>
         
     );
